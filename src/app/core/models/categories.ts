4e2ace78e8c2ae2f4ff1c0e49ae26e2cc0eefb4b
Becaye <baldebecaye10@gmail.com>
2025-01-18 15:43:10 -0500

    Setup gh-pages branch

--- src/app/core/models/categories.ts
+++
@@ -1,6 +0,0 @@
-export enum Categories {
-    DataScience = 'Data Science',
-    SoftwareDevelopment = 'Software Development',
-    SelfGrowth = 'Self Growth',
-    Other = 'Other',
-}
--- src/global.ts
+++
@@ -1,1 +0,0 @@
-export const AVATAR = 'assets/images/avatar.jpg';
